a54c0ae2a54a11ed3c718684777cacaa582c623e
Talissa <liayai@ya.ru>
2024-07-25 14:23:48 +0300

    resolve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let number = Math.abs(n);
+    while (number) {
+        result += number % 10;
+        number = Math.trunc(number / 10);
+    };
+
+    return +result;
 }
